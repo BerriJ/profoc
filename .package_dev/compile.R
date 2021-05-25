1e5135ee0f1feb3d784f0310eadbd8c580e6cc4c
Jonathan Berrisch <jonathan@berrisch.biz>
2021-05-25 10:32:00 +0200

    Update URLs in DESCRIPTION

--- .package_dev/compile.R
+++ .package_dev/compile.R
@@ -3,3 +3,11 @@ devtools::build()
 roxygen2::roxygenize(roclets = "rd")
 devtools::check(env_vars = c("NOT_CRAN" = "false"))
 devtools::test()
+
+# Release TODO:
+
+# Bump Version Number in Description
+# Update the Date field in Description
+# Update the Date in NEWS.md
+# Create a tagged commit
+# Merge develop into main, without squashing commits, wihtout extra merge commit
