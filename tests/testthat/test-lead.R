bc86bef7a53ca7508fb33615f04a9b17d750f232
Jonathan Berrisch <jonathan@berrisch.biz>
2021-06-15 11:15:05 +0200

    Add tests for lead time and singular design error

---
+++ tests/testthat/test-lead.R
@@ -0,0 +1,71 @@
+# %% Test lead for online
+set.seed(1)
+
+# Experts
+N <- 2
+# Observations
+T <- 500
+# Size of probability grid
+P <- 1
+prob_grid <- 1:P / (P + 1)
+
+# Realized observations
+y <- rnorm(T)
+
+dev <- c(-2, 3)
+experts_sd <- c(1, 2)
+
+# Expert predictions
+experts <- array(dim = c(T, P, N))
+
+change_at <- 300
+
+experts[1:(change_at - 1), , 1] <- 5
+experts[1:(change_at - 1), , 2] <- -3
+experts[change_at:T, , 1] <- 500
+experts[change_at:T, , 2] <- -3
+
+theor_opt2 <- (qnorm(prob_grid) - experts[T, , 2]) /
+    (experts[T, , 1] - experts[T, , 2])
+
+init_win <- 5
+lead <- 10
+
+results <- online(
+    matrix(y),
+    experts,
+    prob_grid,
+    lead_time = lead,
+    trace = FALSE
+)
+
+# ts.plot(results$weights[, , 1], col = rainbow(ncol(results$weights)))
+# abline(v = change_at)
+
+expect_gt(results$weights[change_at + 5, , 1], 0.2)
+
+expect_lt(results$weights[change_at + lead + 5, , 1], 0.2)
+
+# %%
+
+# %% Test lead for batch
+results <- batch(
+    matrix(y),
+    experts,
+    prob_grid,
+    affine = TRUE,
+    positive = TRUE,
+    initial_window = init_win,
+    lead_time = lead,
+    expanding_window = FALSE,
+    trace = FALSE
+)
+
+# ts.plot(results$weights[, , 1], col = rainbow(ncol(results$weights)))
+# abline(v = change_at)
+
+expect_gt(results$weights[change_at + 5, , 1], 0.2)
+
+expect_lt(results$weights[change_at + lead + 5, , 1], 0.2)
+
+# %%
--- tests/testthat/test-spline-basis.R
+++ tests/testthat/test-spline-basis.R
@@ -42,7 +42,6 @@ order <- 3
 deg <- order - 1
 a <- sqrt(2)
 pstep <- 0.01
-
 x <- seq(pstep, 1 - pstep, pstep)
 
 # Create desired knots:
@@ -55,3 +54,17 @@ old <- splines::splineDesign(knots, x, deg + 1, derivs = 0L, outer.ok = TRUE)
 new <- splines2_basis(x, knots, deg, range(knots))
 
 expect_equal(old, new, tolerance = 0.00001)
+
+# %% Test if error occurs when design matrix is singular and lambda = 0
+
+a <- 1
+pstep <- 0.1
+x <- seq(pstep, 1 - pstep, pstep)
+lambda <- 0
+ndiff <- 1
+
+expect_error(
+    make_hat_matrix(x, kstep, lambda, ndiff, deg, a),
+    "Spline design matrix singular. Either increase kstep, provide more quantiles or increase lambda."
+)
+# %%
