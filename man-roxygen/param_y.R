ecd3f9c3b793f8b331b6f0a674ba198a6d5375d5
Jonathan Berrisch <jonathan@berrisch.biz>
2021-06-17 09:41:59 +0200

    Update documentation

--- man-roxygen/param_y.R
+++ man-roxygen/param_y.R
@@ -1,1 +1,5 @@
-#' @param y A numeric vector of realizations.
+#' @param y A numeric matrix of realizations. In probabilistic
+#' settings a matrix of dimension Tx1. In multivariate setting
+#' a TxP matrix can be used. In the latter case each slice of
+#' the experts array gets evaluated using the corresponding
+#' column of the y matrix.
