658bfa176be70c72940a0c5374da403b6fc74f3f
Jonathan Berrisch <jonathan@berrisch.biz>
2021-11-12 11:25:59 +0100

    Add test.cpp

---
+++ src/test.cpp
@@ -0,0 +1,40 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppClock)]]
+
+#include <RcppArmadillo.h>
+#include <RcppClock.h>
+#include <thread>
+using namespace arma;
+
+// [[Rcpp::export]]
+void test1()
+{
+
+    Rcpp::Clock clock;
+
+    for (unsigned int x = 0; x < 100; x++)
+    {
+        mat A(1, 99);
+        A.randn();
+        mat B(99, 99);
+        B.randn();
+        mat C;
+        clock.tick("1x99");
+        C = A * B;
+        clock.tock("1x99");
+    }
+
+    for (unsigned int x = 0; x < 100; x++)
+    {
+        mat A(2, 99);
+        A.randn();
+        mat B(99, 99);
+        B.randn();
+        mat C;
+        clock.tick("2x99");
+        C = A * B;
+        clock.tock("2x99");
+    }
+    clock.stop("times");
+    return;
+}
