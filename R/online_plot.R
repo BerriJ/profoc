9d9403df6033323edcc60d81f1f0f9c2a0ee596c
Jonathan Berrisch <jonathan@berrisch.biz>
2021-12-03 12:21:27 +0100

    Update plot method

--- R/online_plot.R
+++ R/online_plot.R
@@ -45,6 +45,7 @@ plot.online <- function(x, ...) {
             lwd = 2,
             cex = 1,
         )
+        readline(prompt = "Showing Plot 1/2. Press [enter] to continue")
     } else if (dx[2] == 1 && dx[3] != 1) {
         # Univariate probabilistic forecasts
         w <- abind::adrop(x$weights, 2)[nrow(x$weights), , ]
@@ -56,7 +57,7 @@ plot.online <- function(x, ...) {
             ylim = c(0, 1),
             ylab = "Weights",
             xlab = "Probability",
-            main = "Weights of the Experts",
+            main = "Most Recent Weights of the Experts",
             xlim = range(idx)
         )
         grid()
@@ -76,9 +77,142 @@ plot.online <- function(x, ...) {
             lwd = 2,
             cex = 1,
         )
+        readline(prompt = "Showing Plot 1/2. Press [enter] to continue")
+    } else if (dx[2] != 1 && dx[3] == 1) {
+        # Univariate probabilistic forecasts
+        w <- abind::adrop(x$weights, 3)[nrow(x$weights), , ]
+        w <- t(apply(w, 1, cumsum))
+        w <- cbind(0, w)
+        idx <- seq_len(nrow(w))
+        cols <- darken(rainbow(n = dim(w)[2]), 1.2)
+        plot(idx, idx * NA,
+            ylim = c(0, 1),
+            ylab = "Weights",
+            xlab = "Probability",
+            main = "Most Recent Weights of the Experts for all Variables",
+            xlim = range(idx)
+        ) # TODO: Replace with bar chart
+        grid()
+        for (i in 2:dim(w)[2]) {
+            polygon(
+                c(idx, rev(idx)),
+                c(w[, i - 1], rev(w[, i])),
+                col = cols[i],
+                border = cols[i],
+                lwd = 1
+            )
+        }
+        legend(
+            min(idx), 1,
+            legend = enames,
+            col = cols[-1],
+            lwd = 2,
+            cex = 1,
+        )
+        readline(prompt = "Showing Plot 1/2. Press [enter] to continue")
+    } else if (dx[2] != 1 && dx[3] != 1) {
+        decision <- 0
+        while (decision != "3") {
+            decision <- readline(prompt = "Choose one of the following:
+        \n(1) Show recent weights vs probabilities
+        \n(2) Show recent weights vs variables
+        \n(3) Skip
+        \nInput:")
+            if (decision == "1") {
+                # Weights vs variables
+                vars <- seq_len(dx[2])
+                # decision_var <- readline(prompt = paste0(
+                #     "Choose a variable: [",
+                #     vars[1], "-",
+                #     vars[length(vars)],
+                #     "] or \nType 'all' to cycle threw them. \nInput:"
+                # ))
+                # if (decision_var == "all") {
+                for (d in vars) {
+                    w <- x$weights[nrow(x$weights), d, , ]
+                    w <- t(apply(w, 1, cumsum))
+                    w <- cbind(0, w)
+                    idx <- seq_len(nrow(w))
+                    cols <- darken(rainbow(n = dim(w)[2]), 1.2)
+                    plot(idx, idx * NA,
+                        ylim = c(0, 1),
+                        ylab = "Weights",
+                        xlab = "",
+                        main = paste0("Recent weights of the Experts for Variable ", d, "."),
+                        xlim = range(idx)
+                    )
+                    grid()
+                    for (i in 2:dim(w)[2]) {
+                        polygon(
+                            c(idx, rev(idx)),
+                            c(w[, i - 1], rev(w[, i])),
+                            col = cols[i],
+                            border = cols[i],
+                            lwd = 1
+                        )
+                    }
+                    legend(
+                        min(idx), 1,
+                        legend = enames,
+                        col = cols[-1],
+                        lwd = 2,
+                        cex = 1,
+                    )
+                    readline(prompt = paste0(
+                        "Showing Plot ", d, "/",
+                        max(vars), ". Press [enter] to continue"
+                    ))
+                    #     }
+                }
+            } else if (decision == "2") {
+                # Weights vs variables
+                probs <- seq_len(dx[3])
+                # decision_var <- readline(prompt = paste0(
+                #     "Choose a variable: [",
+                #     vars[1], "-",
+                #     vars[length(vars)],
+                #     "] or \nType 'all' to cycle threw them. \nInput:"
+                # ))
+                # if (decision_var == "all") {
+                for (p in probs) {
+                    w <- x$weights[nrow(x$weights), , p, ]
+                    w <- t(apply(w, 1, cumsum))
+                    w <- cbind(0, w)
+                    idx <- seq_len(nrow(w))
+                    cols <- darken(rainbow(n = dim(w)[2]), 1.2)
+                    plot(idx, idx * NA,
+                        ylim = c(0, 1),
+                        ylab = "Weights",
+                        xlab = "",
+                        main = paste0("Recent weights of the experts for probability ", p, "."),
+                        xlim = range(idx)
+                    )
+                    grid()
+                    for (i in 2:dim(w)[2]) {
+                        polygon(
+                            c(idx, rev(idx)),
+                            c(w[, i - 1], rev(w[, i])),
+                            col = cols[i],
+                            border = cols[i],
+                            lwd = 1
+                        )
+                    }
+                    legend(
+                        min(idx), 1,
+                        legend = enames,
+                        col = cols[-1],
+                        lwd = 2,
+                        cex = 1,
+                    )
+                    readline(prompt = paste0(
+                        "Showing Plot ", p, "/",
+                        max(probs), ". Press [enter] to continue"
+                    ))
+                    #     }
+                }
+            }
+        }
     }
-
-    readline(prompt = "Showing Plot 1/2. Press [enter] to continue")
 
     loss <- cbind(
         "Combination" = apply(x$forecaster_loss, 1, mean),
