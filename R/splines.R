fec3dfaeabf1133ab13fd869885289d2f5db2386
Jonathan Berrisch <Jonathan@Berrisch.biz>
2023-03-02 07:51:59 +0100

    Switch shape parameters in beta knot distribution
    This is more intuitive as more knots will end up in the lower tail if mu is decreased.

--- R/splines.R
+++ R/splines.R
@@ -1,6 +1,6 @@
 pgbeta <- function(x, mu = .5, sig = 1, nonc = 0) {
-    a <- mu * sig * 2
-    b <- (1 - mu) * sig * 2
+    a <- (1 - mu) * sig * 2
+    b <- mu * sig * 2
     c <- abs(nonc)
     seq_beta <- pbeta(x, a, b, c)
     if (nonc < 0) seq_beta <- rev(1 - seq_beta)
