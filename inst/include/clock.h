3819cbc7e15c5243af50872013c56e771fb6158a
Jonathan Berrisch <Jonathan@Berrisch.biz>
2023-12-13 16:40:32 +0100

    Update clock.h

--- inst/include/clock.h
+++ inst/include/clock.h
@@ -4,6 +4,7 @@
 #include <RcppArmadillo.h>
 #include <chrono>
 #include <string>
+#include <vector>
 #include <map>
 
 #ifndef _OPENMP
@@ -12,6 +13,13 @@ inline int omp_get_thread_num() { return 0; }
 
 namespace Rcpp
 {
+    template <typename T>
+    void remove_duplicates(std::vector<T> &vec)
+    {
+        std::sort(vec.begin(), vec.end());
+        vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+    }
+
     namespace sc = std::chrono;
 
     class Clock
@@ -21,12 +29,13 @@ namespace Rcpp
         using timesmap = std::map<keypair, tp>;
 
     private:
-        std::string name;
         timesmap tickmap;
+
+    public:
+        std::string name;
         std::vector<double> timers;
         std::vector<std::string> names;
 
-    public:
         // Init - Set name of R object
         Clock() : name("times") {}
         Clock(std::string name_) : name(name_) {}
@@ -57,15 +66,51 @@ namespace Rcpp
             }
         }
 
-        // Pass data to R
-        void stop()
+        // Pass data to R / Python
+        void aggregate()
         {
+            // Create copy of names called unique_names
+            std::vector<std::string> unique_names = names;
+            remove_duplicates(unique_names);
+
+            std::vector<std::tuple<std::string, double, int>>
+                table(unique_names.size());
+
+            std::vector<double> averages(unique_names.size());
+            std::vector<int> counts(unique_names.size());
+
+            // Loop over unique names
+            for (unsigned int i = 0; i < unique_names.size(); i++)
+            {
+                int sum = 0;
+                int count = 0;
+
+                // Loop over all names
+                for (unsigned int j = 0; j < names.size(); j++)
+                {
+                    if (names[j] == unique_names[i])
+                    {
+                        sum += timers[j];
+                        count++;
+                    }
+                }
+
+                // Calculate average, convert to milliseconds, round to 3 dec
+                averages[i] = (std::round((sum * 1e-3) / double(count)) / 1e+3);
+                counts[i] = count;
+            }
+
             DataFrame df = DataFrame::create(
-                Named("Name") = names,
-                Named("Nanoseconds") = timers);
-            df.attr("class") = "cppclock";
+                Named("Name") = unique_names,
+                Named("Milliseconds") = averages,
+                Named("Count") = counts);
             Environment env = Environment::global_env();
             env[name] = df;
+        }
+
+        void stop()
+        {
+            aggregate();
         }
 
         // Destructor
